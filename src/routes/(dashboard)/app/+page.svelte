38868390753970328ee2fab5840c3be5ec8cb706
oluwatimilehin awoniyi <awoniyitimi@outlook.com>
2025-08-01 20:11:39 +0100

    set dashboard for development

--- src/routes/(dashboard)/app/+page.svelte
+++ src/routes/(dashboard)/app/+page.svelte
@@ -1,24 +1,4 @@
 <script lang="ts">
 </script>
 
-<section class="flex h-[60vh] flex-col items-center justify-center">
-	<div class="flex flex-col items-center justify-center gap-8 text-center">
-		<div class="rounded-full bg-[#ff4d00]/70 px-4 py-0.5 text-sm font-medium">
-			<p class="text-white">job application assistant</p>
-		</div>
-
-		<h1 class="text-4xl font-bold">Paladin helps you send smart job applications</h1>
-
-		<p class="text-lg text-gray-500">
-			Our assistant will help you create a job application that stands out and increases
-			your chances of getting hired.
-		</p>
-
-		<a
-			href="/login"
-			class="flex h-8 items-center justify-center rounded-md bg-[#ff4d00] p-5 text-xl font-semibold text-white capitalize"
-		>
-			get started
-		</a>
-	</div>
-</section>
+<section class="flex h-[60vh] flex-col items-center justify-center px-4"></section>
